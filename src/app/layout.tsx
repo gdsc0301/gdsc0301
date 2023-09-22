094f77ddc38ee47e8c6ecc245ac7e01e9b32f9d5
Guilherme Carvalho <gdsc0301@gmail.com>
2023-09-22 13:05:27 -0300

    Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Montserrat } from 'next/font/google'
 import { defaultMetadata } from './metadata'
+import { Analytics } from '@vercel/analytics/react'
 
 const montserrat = Montserrat({ subsets: ['latin'] })
 
@@ -11,6 +12,7 @@ export default function RootLayout({children}: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={montserrat.className + ' antialiased'}>{children}</body>
+      <Analytics />
     </html>
   )
 }
